133784c5e279b4aad4ad0c6f76d47adf529afdbe
Steve Bazelais <57912340+SBazelais@users.noreply.github.com>
2023-09-12 13:01:12 -0400

    TEST streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,4 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Smoothie')
-streamlit.text('Hard-boiled eggs')
+streamlit.header('TEST')
