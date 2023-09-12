7ec6c13d68ede92ae467e9b059e7d54aeb088d66
Steve Bazelais <57912340+SBazelais@users.noreply.github.com>
2023-09-12 12:57:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+steamlit.header('Breakfast Menu')
+steamlit.text('Omega 3 & Blueberry Oatmeal')
+steamlit.text('Kale, Spinach & Smoothie')
+steamlit.text('Hard-boiled eggs')
